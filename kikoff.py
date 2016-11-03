3094d46ec561bfa2857456218815202a7b10b657
Daniel Laube <mail@dlaube.de>
2016-11-03 15:34:13 +0100

    Improved kickoff.py by making it more object oriented

--- kikoff.py
+++ kikoff.py
@@ -6,58 +6,70 @@ import pygame
 import sys
 
 
-"""
-Initialisierungsfunktion. Wird einmal beim Programmstart ausgefuehrt"""
-def init():
-    toLoad = [
-        ("mainchar-r", "images/mainchar-r.png"),
-        ("mainchar-l", "images/mainchar-l.png"),
-        ("mainchar-n", "images/mainchar-n.png"),
-        ("mainchar-j", "images/mainchar-j.png"),
-        ("ball", "images/ball.png")
-    ]
-    for asset in toLoad:
-        # Lade alle Assets in der Liste mit Lazy Loading
-        assetManager.load(asset[0], asset[1], True)
+class Game(object):
+    """
+        Initialisierungsmethode. Wird einmal beim Programmstart ausgefuehrt
+    """
+    def __init__(self):
+        toLoad = [
+            ("mainchar-r", "images/mainchar-r.png"),
+            ("mainchar-l", "images/mainchar-l.png"),
+            ("mainchar-n", "images/mainchar-n.png"),
+            ("mainchar-j", "images/mainchar-j.png"),
+            ("ball", "images/ball.png")
+        ]
+        for asset in toLoad:
+            # Lade alle Assets in der Liste mit Lazy Loading
+            assetManager.load(asset[0], asset[1], True)
 
-    # Fuege einen Hauptcharacter zum Spielfeld hinzu
-    entityManager.addEntity(entities.MainCharacter())
+        # Fuege einen Hauptcharacter zum Spielfeld hinzu
+        entityManager.addEntity(entities.MainCharacter())
 
-"""
-Update Funktion, aktualisiert alle Entities
-"""
-def update(delta):
-    entityManager.update(delta)
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+        self.time_a = time.time()
+        self.time_b = time.time()
 
-"""
-Render Funktion, rendert alle Entities
-"""
-def render(screen):
-    screen.fill((0, 0, 0))
-    entityManager.render(screen)
+        
+    """
+        Update Methode, aktualisiert alle Entities
+    """
+    def update(self, delta):
+        entityManager.update(delta)
+        
+
+    """
+        Render Methode, rendert alle Entities
+    """
+    def render(self):
+        self.screen.fill((0, 0, 0))
+        entityManager.render(self.screen)
+
+    """
+        Die Loop
+    """
+    def loop(self):
+        # Errechne Zeitdifferenz
+        self.time_b = self.time_a
+        self.time_a = time.time()
+        # Fuehre Update Aus
+        self.update(self.time_a - self.time_b)
+        # Fuehre Render aus
+        self.render()
+        # Flippe den Screen Buffer
+        pygame.display.flip()
+        self.clock.tick(60)
+        # Regiere auf Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
-# Pygame Initialisierung
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-time_a = time.time()
-time_b = time.time()
-# Fuehre Init aus
-init()
+
+
+game = Game()
 while True:
-    # Errechne Zeitdifferenz
-    time_b = time_a
-    time_a = time.time()
-    # Fuehre Update Aus
-    update(time_a - time_b)
-    # Fuehre Render aus
-    render(screen)
-    # Flippe den Screen Buffer
-    pygame.display.flip()
-    clock.tick(60)
-    # Regiere auf Events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+    game.loop()
+    
 
